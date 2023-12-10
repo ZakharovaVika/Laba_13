f28b4ff7b366dbea2f14adadaad06d1ced2015cd
ZakharovaVika <125094864+ZakharovaVika@users.noreply.github.com>
2023-12-10 18:02:49 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+import csv
+filename = open('Titanic.csv', 'r')
+file = csv.DictReader(filename)
+def median(age):
+    n = len(age)
+    index = n // 2
+    if n % 2:
+        return sorted(age)[index]
+    return sum(sorted(age)[index - 1:index + 1]) / 2
+
+
+age = []
+total = []
+for col in file:
+    if col['sex'] == 'female':
+        if col['age'] != '':
+            age.append(col['age'])
+            data = {
+                'age': col['age'],
+                'class': col['class']
+            }
+            total.append(data)
+res = int(median(age))
+money = 0
+for i in total:
+    if res - 5 <= float(i['age']) <= res + 5:
+        if i['class'] == 'First':
+            money += 150
+        if i['class'] == 'Second':
+            money += 60
+        else:
+            money += 30
+print(f'Cуммарная стоимость билетов женщин на борту '
+      f'в возрастном интервале медиана ± 5 лет составила : {money}$.')
+
+
+
+
+
